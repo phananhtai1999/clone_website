394d6148714dacb422dade6325bb84df8b9b5f11
phananhtai1999 <150696532+phananhtai1999@users.noreply.github.com>
2024-07-29 23:40:16 +0700

    push code

---
+++ Admin_Dashboard_2/assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".xp-menubar").on("click", function () {
+        $("#sidebar").toggleClass("active");
+        $("#content").toggleClass("active");
+    });
+
+    $(".xp-menubar,.body-overlay").on("click", function () {
+        $("#sidebar,.body-overlay").toggleClass("show-nav");
+    });
+});
---
+++ sidebar_menu_2/main.js
@@ -0,0 +1,4 @@
+const toggle = document.querySelector(".btn");
+toggle.addEventListener("click", function () {
+    document.querySelector("#sidebar").classList.toggle("expand");
+});
